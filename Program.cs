b65928db2f1e01f762d38a1f8225b9cf4a12e606
Daniil <daniil.goihinberg@gmail.com>
2022-05-26 20:26:28 +0300

    homework

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MatterOfTaste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] color = { "red", "green", "blue" };
+            Console.WriteLine("Enter your favotrite color:");
+            string UserColor = Console.ReadLine();
+
+            for (int i = 0; i < color.Length; i++)
+            {
+                if (UserColor == color[0])
+                {
+                    Console.WriteLine("We are perfect together");
+                }
+                else if (UserColor == color[1])
+                {
+                    Console.WriteLine("We are perfect together");
+                }
+                else if (UserColor == color[2])
+                {
+                    Console.WriteLine("We are perfect together");
+                } else
+                {
+                    Console.WriteLine("Every man to his taste");
+                } break;
+            }
+        }
+    }
+}
